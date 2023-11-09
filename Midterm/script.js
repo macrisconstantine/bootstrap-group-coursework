9a98ee2fecf00a38b893b2c2105bcdab483c99b9
macrisconstantine <c.macris@acg.edu>
2023-11-09 23:58:17 +0200

    Add table update, delete, and sort functionality plus some more formatting

--- Midterm/script.js
+++ Midterm/script.js
@@ -1,7 +1,94 @@
+$(document).ready(function() {
+    // Define an array to store data
+    const data = [];
+    // const dataTable = $('#gameList').DataTable({
+    //     searching: false, // Disable search bar
+    //     paging: false, // Enable pagination
+    //     ordering: true, // Enable sorting
+    //     info: false, // Disable showing "Showing x of y entries"
+        
+    //     language: {
+    //         emptyTable: "", // Set the empty table message to an empty string
+    //         zeroRecords: ""
+    //     } 
+
+    // });
+
+    // Select elements
+    const dataForm = $('#dataForm');
+    const dataList = $('#dataList');
+  
+    dataForm.submit(function(event) {
+      event.preventDefault();
+      const game = $('#game').val();
+      const rating = $('#rating').val();
+      data.push({ game, rating });
+      appendRow(data.length, game, rating);
+      dataForm[0].reset();
+      dataTable.clear().rows.add(data).draw();
+    });
+
+    function appendRow(index, game, rating) {
+      dataList.append(`
+        <tr>
+            <td>${index}</td>
+            <td>${game}</td>
+            <td>${rating}</td>
+            <td><button class="btn btn-danger delete">Delete</button></td>
+        </tr>
+      `);
+    }
+
+    dataList.on('click', '.delete', function() {
+      const row = $(this).closest('tr');
+      const index = row.index();
+      data.splice(index, 1);
+      row.remove();
+
+      updateIndexColumn();
+      dataTable.clear().rows.add(data).draw();
+    });
+
+    function updateIndexColumn() {
+        dataList.find('tr').each(function(index) {
+          $(this).find('td:first').text(index + 1); // Update the index column
+        });
+    }
+
+    // Sort the data array by game name
+    function sortDataByGame() {
+        data.sort((a, b) => a.game.localeCompare(b.game));
+        updateTable();
+      }
+  
+      // Sort the data array by rating
+      function sortDataByRating() {
+        data.sort((a, b) => b.rating - a.rating);
+        updateTable();
+      }
+  
+      // Function to update the table based on the sorted data
+      function updateTable() {
+        dataList.empty(); // Clear the existing table rows
+        data.forEach((item, index) => {
+          appendRow(index + 1, item.game, item.rating);
+        });
+      }
+  
+      // Example: Call the sort functions when clicking on table headers
+      $('#gameList th:nth-child(2)').on('click', function() {
+        sortDataByGame();
+      });
+  
+      $('#gameList th:nth-child(3)').on('click', function() {
+        sortDataByRating();
+      });
+});
+
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    var topnav=document.getElementById("TT");
-    var dropdown=document.getElementById("POP");
+    var topnav=document.getElementById("menu-bar");
+    var dropdown=document.getElementById("menu-btn");
 
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 120) { // if scolling is detected then:
         topnav.style.transform= "scale(0)"; // scale top navigation bar to 0 and 
@@ -18,6 +105,10 @@ function scrollFunction() {
         topnav.style.opacity=1; //and opacity to 1 to give the illsuion of a very basic animation
     }
 }
+
+function runningFormatter(value, row, index) {
+    return index+1;
+    }
 
 
 
